5a940f8c09e9c28e4222e258206820858ec0edd7
Ng Van Duy <nvd.hy98@gmail.com>
2023-05-28 21:24:51 +0700

    Update NotificationUICanvas.cs

--- Assets/Script/UI/NotificationUICanvas.cs
+++ Assets/Script/UI/NotificationUICanvas.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class NotificationUICanvas : UICanvas
 {
     public Text apologizeText;
-    public string txt = "I apologize for submitting late due to an unexpected reason. The 'Extra Asset' folder is not related to the project, I used it to get models for the game.\nUse A W D S move and jump and slide.";
+    private string txt = "I apologize for submitting late due to an unexpected reason. The 'Extra Asset' folder is not related to the project, I used it to get models for the game. Use A W D S move and jump and slide.";
     public override void Show()
     {
         base.Show();
